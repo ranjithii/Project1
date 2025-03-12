e1bca675e9541aeac6f9a85d7e24d4f5a8770be0
ranjithii <indranstars@gmail.com>
2025-03-12 05:13:10 -0400

    Delete cloudwatch.tf

--- cloudwatch.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_group" "app_log_group" {
-  name = "/aws/lambda/app-log-group"
-}
-
-resource "aws_cloudwatch_log_stream" "app_log_stream" {
-  log_group_name = aws_cloudwatch_log_group.app_log_group.name
-  name           = "app-log-stream"
-}
